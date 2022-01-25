1130bea5f5f8e3b37d0bd30cd67386c4c0c0e05f
hadifayad9 <hadifayad9@hotmail.com>
2022-01-25 13:55:23 -0500

    Basic setup.
    Navbar,
    Server,
    Postgres Integ,
    API post req,
    Bootstrap,

---
+++ src/views/homepage/HomePage.vue
@@ -0,0 +1,17 @@
+/* eslint-disable */
+<template>
+  <div>
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "../../components/NavBar/NavBar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style scoped></style>
